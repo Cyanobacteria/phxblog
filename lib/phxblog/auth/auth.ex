e8097712492257174b679d0a442ad8d8c8307403
Cyanobactteria <vodomelkor@gmail.com>
2018-10-27 00:26:55 +0800

    add session controller and auth ecto

---
+++ lib/phxblog/auth/auth.ex
@@ -0,0 +1,15 @@
+defmodule Phxblog.Auth do
+  alias Phxblog.Repo
+  alias Phxblog.Auth.User
+  
+  def sign_in(email, password) do
+    user = Repo.get_by(User, email: email)
+
+    cond do
+      user && user.encrypted_password == password ->
+        {:ok, user}
+      true ->
+        {:error, :unauthorized}
+    end
+  end
+end
