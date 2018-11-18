949d692c15721d2f8cbb5f03a117c22018237cc8
Cyanobactteria <vodomelkor@gmail.com>
2018-11-18 19:43:05 +0800

    modify schema post

--- lib/phxblog/post.ex
+++ lib/phxblog/post.ex
@@ -5,15 +5,9 @@ defmodule Phxblog.Post do
 
   schema "posts" do
     field :context, :string
-    field :create_time, :naive_datetime
-    field :create_user, :string
-    field :delete_time, :naive_datetime
-    field :delete_user, :string
-    field :is_available, :integer
+    field :delete_time, :integer
     field :memo, :string
     field :title, :string
-    field :update_time, :naive_datetime
-    field :update_user, :string
     field :user_id, :integer
 
     timestamps()
@@ -22,7 +16,7 @@ defmodule Phxblog.Post do
   @doc false
   def changeset(post, attrs) do
     post
-    |> cast(attrs, [:title, :context, :user_id, :memo, :is_available, :create_user, :create_time, :update_user, :update_time, :delete_user, :delete_time])
-    |> validate_required([:title, :context, :user_id, :memo, :is_available, :create_user, :create_time, :update_user, :update_time, :delete_user, :delete_time])
+    |> cast(attrs, [:title, :context, :user_id, :memo, :delete_time])
+    |> validate_required([:title, :context, :user_id, :memo, :delete_time])
   end
 end
